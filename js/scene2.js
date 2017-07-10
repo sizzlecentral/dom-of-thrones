9e3429befc4604711d2badc756c77e19801f7f5a
Don Cooper <don_cooper@hotmail.com>
2017-07-10 13:06:27 -0400

    Scene 2 file created and tested

---
+++ js/scene2.js
@@ -0,0 +1,33 @@
+function scene2() {
+
+  console.log('Executing scene 2!');
+
+  // Setup Stage
+
+  // Setup Cast
+
+  // Character 1
+
+  // Character 2
+
+  // Setup Props
+
+  // Prop 1
+
+  // Prop 2
+
+  // Action!
+
+  // Stage direction 1
+
+  frame(function() {
+
+  })
+
+  // Stage direction 2
+
+  frame(function() {
+
+  })
+
+}
