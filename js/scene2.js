a1b8dcfb8ac3f08179eccbd299600c2b4a3d8554
Don Cooper <don_cooper@hotmail.com>
2017-07-10 13:10:40 -0400

    Stage set for scene 2

--- js/scene2.js
+++ js/scene2.js
@@ -3,6 +3,9 @@ function scene2() {
   console.log('Executing scene 2!');
 
   // Setup Stage
+  stage = document.querySelector('#stage');
+  castleImgSrc = castle.getAttribute('src');
+  stageImg.setAttribute('src', castleImgSrc);
 
   // Setup Cast
 
