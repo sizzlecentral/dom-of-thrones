8cfbe7f9f9f2f51f7f08e47d83a7648662ea081a
Don Cooper <don_cooper@hotmail.com>
2017-07-10 13:29:18 -0400

    Sword animation done

--- js/scene2.js
+++ js/scene2.js
@@ -1,7 +1,5 @@
 function scene2() {
 
-  console.log('Executing scene 2!');
-
   // Setup Stage
   stage = document.querySelector('#stage');
   castleImgSrc = castle.getAttribute('src');
@@ -10,12 +8,52 @@ function scene2() {
   // Setup Cast
 
   // Character 1
+  nedContainer = document.createElement('div');
+  stage.append(nedContainer);
+  nedContainer.style.position = 'absolute';
+  nedContainer.style.width = '10%';
+  nedContainer.style.height = '37%';
+  nedContainer.style.top = '55%';
+  nedContainer.style.left = '25%';
+  ned = document.querySelector('#ned');
+  nedContainer.append(ned);
+  leather = document.querySelector('#leather-armour');
+  nedContainer.append(leather);
+  ned.style.width = '70%';
+  ned.style.position = 'absolute';
+  ned.style.top = 0;
+  leather.style.position = 'absolute';
+  leather.style.bottom = 0;
 
   // Character 2
+  joffContainer = document.createElement('div');
+  joffContainer.id = 'joff-container';
+  stage.append(joffContainer);
+  joffContainer.style.width = '8%';
+  joffContainer.style.position = 'absolute';
+  joffContainer.style.height = '31%';
+  joffContainer.style.top = '55%';
+  joffContainer.style.left = '55%';
+  joff = document.querySelector('#joffrey');
+  joffContainer.append(joff);
+  joff.style.width = '75%';
+  joff.style.position = 'absolute';
+  joff.style.left = '33%';
+  kingWardrobe = document.querySelector('#king');
+  joffContainer.append(kingWardrobe);
+  kingWardrobe.style.position = 'absolute';
+  kingWardrobe.style.bottom = 0;
 
   // Setup Props
 
   // Prop 1
+  sword = document.querySelector('#sword');
+  stage.append(sword);
+  sword.style.position = 'absolute';
+  sword.style.top = '77%';
+  sword.style.left = '31%';
+  sword.style.height = '5%';
+  sword.style.transform = 'rotateY(150deg)';
 
   // Prop 2
 
@@ -31,6 +69,31 @@ function scene2() {
 
   frame(function() {
 
+  })
+
+  frame(function() {
+    sword.style.top = '77%'
+    sword.style.left = '31%'
+  })
+
+  frame(function() {
+    sword.style.top = '74%'
+    sword.style.left = '36%'
+  })
+
+  frame(function() {
+    sword.style.top = '71%'
+    sword.style.left = '41%'
+  })
+
+  frame(function() {
+    sword.style.top = '68%'
+    sword.style.left = '47%'
+  })
+
+  frame(function() {
+    sword.style.top = '64%'
+    sword.style.left = '53%'
   })
 
 }
